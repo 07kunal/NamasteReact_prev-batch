e2f04cfe6982ea8a3193c35bbd99fa9eaf53c8c0
kunal <kunalgautam200@gmail.com>
2023-07-22 21:18:43 +0530

    update item into new branch

---
+++ chap06_exploring_world/src/config/Constant.js
@@ -0,0 +1,549 @@
+export const IMG_URL = "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
+
+
+export const Restaurant_Data = [
+
+    {
+        type: "restaurant",
+        data: {
+            type: "F",
+            id: "9483",
+            name: "Biryani Blues",
+            uuid: "e5a9993e-b013-42cd-8ba5-c33771221f6e",
+            city: "4",
+            area: "Dwarka",
+            totalRatingsString: "10000+ ratings",
+            cloudinaryImageId: "ps5pmfgdnfteakim0gc4",
+            cuisines: [
+                "Biryani",
+                "Hyderabadi",
+                "Lucknowi",
+                "Kebabs",
+                "Desserts",
+                "Beverages"
+            ],
+            tags: [],
+            costForTwo: 40000,
+            costForTwoString: "₹400 FOR TWO",
+            deliveryTime: 29,
+            minDeliveryTime: 29,
+            maxDeliveryTime: 29,
+            slaString: "29 MINS",
+            lastMileTravel: 5,
+            slugs: {
+                restaurant: "biryani-blues-dwarka-dwarka",
+                city: "delhi"
+            },
+            cityState: "4",
+            address: "- 203, 2nd floor, Bhagwati Plaza, Plot o.12, Sec 5, Dwarka, New Delhi.- 110075",
+            locality: "Dwarka",
+            parentId: 13813,
+            unserviceable: false,
+            veg: false,
+            select: false,
+            favorite: false,
+            tradeCampaignHeaders: [],
+            ribbon: [
+                {
+                    type: "PROMOTED"
+                }
+            ],
+            chain: [],
+            feeDetails: {
+                fees: [
+                    {
+                        name: "distance",
+                        fee: 4800,
+                        message: ""
+                    },
+                    {
+                        name: "time",
+                        fee: 0,
+                        message: ""
+                    },
+                    {
+                        name: "special",
+                        fee: 0,
+                        message: ""
+                    }
+                ],
+                totalFees: 4800,
+                message: "",
+                title: "Delivery Charge",
+                amount: "4800",
+                icon: ""
+            },
+            availability: {
+                opened: true,
+                nextOpenMessage: "",
+                nextCloseMessage: ""
+            },
+            longDistanceEnabled: 0,
+            rainMode: "NONE",
+            thirdPartyAddress: false,
+            thirdPartyVendor: "",
+            adTrackingID: "cid=7025970~p=1~eid=00000188-c31c-35d8-09d9-33bc006a0132",
+            badges: {
+                imageBased: [],
+                textBased: [],
+                textExtendedBadges: []
+            },
+            lastMileTravelString: "5 kms",
+            hasSurge: false,
+            aggregatedDiscountInfoV3: {
+                header: "₹125 OFF",
+                subHeader: "ABOVE ₹249",
+                discountTag: "FLAT DEAL",
+                headerTypeV2: 0
+            },
+            sla: {
+                restaurantId: "9483",
+                deliveryTime: 29,
+                minDeliveryTime: 29,
+                maxDeliveryTime: 29,
+                lastMileTravel: 5,
+                lastMileDistance: 0,
+                serviceability: "SERVICEABLE",
+                rainMode: "NONE",
+                longDistance: "NOT_LONG_DISTANCE",
+                preferentialService: false,
+                iconType: "EMPTY"
+            },
+            promoted: true,
+            avgRating: "4.3",
+            totalRatings: 10000,
+            new: false
+        },
+        subtype: "basic"
+    },
+    {
+        type: "restaurant",
+        data: {
+            type: "F",
+            id: "28451",
+            name: "Grover Sweets & Bakers",
+            uuid: "f4787efb-89ca-4954-bf69-df8f83cec00b",
+            city: "4",
+            area: "Uttam Nagar",
+            totalRatingsString: "10000+ ratings",
+            cloudinaryImageId: "at6kqassx47vpnzcpvwm",
+            cuisines: [
+                "North Indian",
+                "Sweets",
+                "Street Food",
+                "Snacks",
+                "Indian",
+                "Tandoor",
+                "Desserts"
+            ],
+            tags: [],
+            costForTwo: 30000,
+            costForTwoString: "₹300 FOR TWO",
+            deliveryTime: 22,
+            minDeliveryTime: 22,
+            maxDeliveryTime: 22,
+            slaString: "22 MINS",
+            lastMileTravel: 2,
+            slugs: {
+                restaurant: "grover-sweets-bakers-uttam-nagar-uttam-nagar",
+                city: "delhi"
+            },
+            cityState: "4",
+            address: "Plot no -1, Diwan Estate, Bhagwati Garden, Near Dwarka Mod Metro Station, Uttam Nagar, New Delhi",
+            locality: "Bhagwati Garden",
+            parentId: 91011,
+            unserviceable: false,
+            veg: true,
+            select: false,
+            favorite: false,
+            tradeCampaignHeaders: [],
+            chain: [],
+            feeDetails: {
+                fees: [
+                    {
+                        name: "distance",
+                        fee: 3200,
+                        message: ""
+                    },
+                    {
+                        name: "time",
+                        fee: 0,
+                        message: ""
+                    },
+                    {
+                        name: "special",
+                        fee: 0,
+                        message: ""
+                    }
+                ],
+                totalFees: 3200,
+                message: "",
+                title: "Delivery Charge",
+                amount: "3200",
+                icon: ""
+            },
+            availability: {
+                opened: true,
+                nextOpenMessage: "",
+                nextCloseMessage: ""
+            },
+            longDistanceEnabled: 0,
+            rainMode: "NONE",
+            thirdPartyAddress: false,
+            thirdPartyVendor: "",
+            adTrackingID: "",
+            badges: {
+                imageBased: [],
+                textBased: [],
+                textExtendedBadges: []
+            },
+            lastMileTravelString: "2 kms",
+            hasSurge: false,
+            aggregatedDiscountInfoV3: {
+                header: "40% OFF",
+                subHeader: "UPTO ₹80",
+                discountTag: "",
+                headerTypeV2: 0
+            },
+            sla: {
+                restaurantId: "28451",
+                deliveryTime: 22,
+                minDeliveryTime: 22,
+                maxDeliveryTime: 22,
+                lastMileTravel: 2,
+                lastMileDistance: 0,
+                serviceability: "SERVICEABLE",
+                rainMode: "NONE",
+                longDistance: "NOT_LONG_DISTANCE",
+                preferentialService: false,
+                iconType: "EMPTY"
+            },
+            promoted: false,
+            avgRating: "4.2",
+            totalRatings: 10000,
+            new: false
+        },
+        subtype: "basic"
+    },
+    {
+        type: "restaurant",
+        data: {
+            type: "F",
+            id: "36694",
+            name: "Sargam Sweets & Restaurant",
+            uuid: "17e6ee59-7045-47a1-b902-66c2b1f8546a",
+            city: "4",
+            area: "Uttam Nagar",
+            totalRatingsString: "10000+ ratings",
+            cloudinaryImageId: "qetsckyabfqyq3qz4opm",
+            cuisines: [
+                "Sweets",
+                "Snacks"
+            ],
+            tags: [],
+            costForTwo: 30000,
+            costForTwoString: "₹300 FOR TWO",
+            deliveryTime: 25,
+            minDeliveryTime: 25,
+            maxDeliveryTime: 25,
+            slaString: "25 MINS",
+            lastMileTravel: 2.700000047683716,
+            slugs: {
+                restaurant: "sargam-sweets-uttam-nagar-uttam-nagar",
+                city: "delhi"
+            },
+            cityState: "4",
+            address: "A 11, Mohan Garden, Near Metro Pillar 753, Peepal Wala Road, Uttam Nagar, New Delhi",
+            locality: "Pipal Wala Road",
+            parentId: 16038,
+            unserviceable: false,
+            veg: false,
+            select: false,
+            favorite: false,
+            tradeCampaignHeaders: [],
+            chain: [],
+            feeDetails: {
+                fees: [
+                    {
+                        name: "distance",
+                        fee: 3200,
+                        message: ""
+                    },
+                    {
+                        name: "time",
+                        fee: 0,
+                        message: ""
+                    },
+                    {
+                        name: "special",
+                        fee: 0,
+                        message: ""
+                    }
+                ],
+                totalFees: 3200,
+                message: "",
+                title: "Delivery Charge",
+                amount: "3200",
+                icon: ""
+            },
+            availability: {
+                opened: true,
+                nextOpenMessage: "",
+                nextCloseMessage: ""
+            },
+            longDistanceEnabled: 0,
+            rainMode: "NONE",
+            thirdPartyAddress: false,
+            thirdPartyVendor: "",
+            adTrackingID: "",
+            badges: {
+                imageBased: [],
+                textBased: [],
+                textExtendedBadges: []
+            },
+            lastMileTravelString: "2.7 kms",
+            hasSurge: false,
+            aggregatedDiscountInfoV3: {
+                header: "₹125 OFF",
+                subHeader: "ABOVE ₹249",
+                discountTag: "FLAT DEAL",
+                headerTypeV2: 0
+            },
+            sla: {
+                restaurantId: "36694",
+                deliveryTime: 25,
+                minDeliveryTime: 25,
+                maxDeliveryTime: 25,
+                lastMileTravel: 2.700000047683716,
+                lastMileDistance: 0,
+                serviceability: "SERVICEABLE",
+                rainMode: "NONE",
+                longDistance: "NOT_LONG_DISTANCE",
+                preferentialService: false,
+                iconType: "EMPTY"
+            },
+            promoted: false,
+            avgRating: "3.7",
+            totalRatings: 10000,
+            new: false
+        },
+        subtype: "basic"
+    },
+    {
+        type: "restaurant",
+        data: {
+            type: "F",
+            id: "11436",
+            name: "Subway",
+            uuid: "0f451c59-d880-4f27-9728-fb0767e6581c",
+            city: "4",
+            area: "Vikas puri",
+            totalRatingsString: "10000+ ratings",
+            cloudinaryImageId: "0bf19a82b109b40c2f5c56d00f071a33",
+            cuisines: [
+                "Salads",
+                "Snacks",
+                "Desserts",
+                "Beverages"
+            ],
+            tags: [],
+            costForTwo: 35000,
+            costForTwoString: "₹350 FOR TWO",
+            deliveryTime: 38,
+            minDeliveryTime: 38,
+            maxDeliveryTime: 38,
+            slaString: "38 MINS",
+            lastMileTravel: 7.599999904632568,
+            slugs: {
+                restaurant: "subway-janakpuri-4",
+                city: "delhi"
+            },
+            cityState: "4",
+            address: "KG1/97 Vikaspuri, PVR Road Vikaspuri",
+            locality: "Pvr Road",
+            parentId: 2,
+            unserviceable: false,
+            veg: false,
+            select: false,
+            favorite: false,
+            tradeCampaignHeaders: [],
+            ribbon: [
+                {
+                    type: "PROMOTED"
+                }
+            ],
+            chain: [],
+            feeDetails: {
+                fees: [
+                    {
+                        name: "distance",
+                        fee: 6400,
+                        message: ""
+                    },
+                    {
+                        name: "time",
+                        fee: 0,
+                        message: ""
+                    },
+                    {
+                        name: "special",
+                        fee: 0,
+                        message: ""
+                    }
+                ],
+                totalFees: 6400,
+                message: "",
+                title: "Delivery Charge",
+                amount: "6400",
+                icon: ""
+            },
+            availability: {
+                opened: true,
+                nextOpenMessage: "",
+                nextCloseMessage: ""
+            },
+            longDistanceEnabled: 0,
+            rainMode: "NONE",
+            thirdPartyAddress: false,
+            thirdPartyVendor: "",
+            adTrackingID: "cid=7123549~p=4~eid=00000188-c31c-35d8-09d9-33bd006a0433",
+            badges: {
+                imageBased: [],
+                textBased: [],
+                textExtendedBadges: []
+            },
+            lastMileTravelString: "7.5 kms",
+            hasSurge: false,
+            aggregatedDiscountInfoV3: {
+                header: "20% OFF",
+                subHeader: "UPTO ₹100",
+                discountTag: "",
+                headerTypeV2: 0
+            },
+            sla: {
+                restaurantId: "11436",
+                deliveryTime: 38,
+                minDeliveryTime: 38,
+                maxDeliveryTime: 38,
+                lastMileTravel: 7.599999904632568,
+                lastMileDistance: 0,
+                serviceability: "SERVICEABLE",
+                rainMode: "NONE",
+                longDistance: "NOT_LONG_DISTANCE",
+                preferentialService: false,
+                iconType: "EMPTY"
+            },
+            promoted: true,
+            avgRating: "3.8",
+            totalRatings: 10000,
+            new: false
+        },
+        subtype: "basic"
+    },
+    {
+        type: "restaurant",
+        data: {
+            type: "F",
+            id: "399890",
+            name: "Pizza Wings",
+            uuid: "32ca4ccd-456c-474d-9818-ab5c4b21a02b",
+            city: "4",
+            area: "Uttam Nagar",
+            totalRatingsString: "1000+ ratings",
+            cloudinaryImageId: "a46a6aa00709f771fabb45c864a81b5e",
+            cuisines: [
+                "Pizzas",
+                "Italian",
+                "Mexican",
+                "American"
+            ],
+            tags: [],
+            costForTwo: 30000,
+            costForTwoString: "₹300 FOR TWO",
+            deliveryTime: 26,
+            minDeliveryTime: 26,
+            maxDeliveryTime: 26,
+            slaString: "26 MINS",
+            lastMileTravel: 2.299999952316284,
+            slugs: {
+                restaurant: "pizza-wings-uttam-nagar-uttam-nagar",
+                city: "delhi"
+            },
+            cityState: "4",
+            address: "GROUND FLOOR WITHOUT ROOF RIGHTS OF PROP NO.-C1, 1A RAMA PARK UTTAM NAGAR-110059, DWARKA, South West   , Delhi-110059",
+            locality: "Rama Park",
+            parentId: 159048,
+            unserviceable: false,
+            veg: false,
+            select: false,
+            favorite: false,
+            tradeCampaignHeaders: [],
+            chain: [],
+            feeDetails: {
+                fees: [
+                    {
+                        name: "distance",
+                        fee: 3200,
+                        message: ""
+                    },
+                    {
+                        name: "time",
+                        fee: 0,
+                        message: ""
+                    },
+                    {
+                        name: "special",
+                        fee: 0,
+                        message: ""
+                    }
+                ],
+                totalFees: 3200,
+                message: "",
+                title: "Delivery Charge",
+                amount: "3200",
+                icon: ""
+            },
+            availability: {
+                opened: true,
+                nextOpenMessage: "",
+                nextCloseMessage: ""
+            },
+            longDistanceEnabled: 0,
+            rainMode: "NONE",
+            thirdPartyAddress: false,
+            thirdPartyVendor: "",
+            adTrackingID: "",
+            badges: {
+                imageBased: [],
+                textBased: [],
+                textExtendedBadges: []
+            },
+            lastMileTravelString: "2.2 kms",
+            hasSurge: false,
+            aggregatedDiscountInfoV3: {
+                header: "₹125 OFF",
+                subHeader: "ABOVE ₹199",
+                discountTag: "FLAT DEAL",
+                headerTypeV2: 0
+            },
+            sla: {
+                restaurantId: "399890",
+                deliveryTime: 26,
+                minDeliveryTime: 26,
+                maxDeliveryTime: 26,
+                lastMileTravel: 2.299999952316284,
+                lastMileDistance: 0,
+                serviceability: "SERVICEABLE",
+                rainMode: "NONE",
+                longDistance: "NOT_LONG_DISTANCE",
+                preferentialService: false,
+                iconType: "EMPTY"
+            },
+            promoted: false,
+            avgRating: "4.0",
+            totalRatings: 1000,
+            new: false
+        },
+        subtype: "basic"
+    }
+]
---
+++ chap06_exploring_world/src/pages/body/Body.js
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import './body.css'
+import { Restaurant_Data } from '../../config/Constant';
+import RestrauntCart from '../../component/RestrauntCart';
+
+
+//config driven UI ( which is build by Backend developer)
+
+
+
+// console.log(Restaurant_Data)
+
+
+
+const filerData = (searchTxt, restrauList) => {
+  
+    return restrauList?.filter((restrau) => restrau?.data?.name?.includes(searchTxt))
+
+
+}
+
+function Body() {
+    let keysearch = "hello test" // writing  local variable in JS 
+
+    // creating the local state variable in React using useState hooks ( which is nothing but the Normal JS function which provide specific functionality.)
+    // changes occur here.
+
+    const [searchTxt, setSearchTxt] = useState('');
+    const [restrau_data, setRestrau_data] = useState(Restaurant_Data)
+
+    return (
+        <>
+            <div className="search_bar">
+                <input type="text" className='search-input' placeholder='search' value={searchTxt}
+                    onChange={(e) => {
+                        setSearchTxt(e.target.value)
+
+                    }
+                    }
+                />
+                <button className="search-btn" onClick={() => {
+                    // need filter data , and pass into updated restrau list
+                    const data = filerData(searchTxt, Restaurant_Data)
+                    console.log(data)
+                    setRestrau_data(data)
+                }}
+
+                >search</button>
+            </div>
+            <div className='Body_main'>
+
+
+                {
+                    // Restaurant_Data?.filter((restrau) => restrau?.data?.name?.toLowerCase().includes(searchTxt.toLowerCase()))?.map((restaurant) 
+                    restrau_data.map((restaurant) => <RestrauntCart key={restaurant?.data?.id} {...restaurant.data} />)
+
+
+
+                    //data pass as destructing form
+                }
+
+
+
+            </div>
+        </>
+
+    )
+}
+
+export default Body
